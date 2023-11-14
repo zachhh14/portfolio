925d1d0afbf0f748538cb27e343017ffd155dffd
zachary tecson <zactec115@gmail.com>
2023-11-15 05:57:03 +0800

    update position

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -12,13 +12,13 @@ function Experience() {
       </h1>
       <div className='flex flex-col lg:flex-row justify-center items-center mt-6 lg:space-x -3 > * lg:mt-20 text-black'>
         <ExperienceCard
-          position='Full-stack Web Developer (Part-time)'
+          position='Full-stack Web Developer'
           company='Technomancer Inc.'
           date='Feb 2023 - Sep 2023'
           description='Assigned to creating business solutions for business like, Employee Maintenance, Payroll System, E-commerce Website'
         />
         <ExperienceCard
-          position='Front-end Web Developer (Intern)'
+          position='Front-end Web Developer'
           company='Hacktiv Colab Inc.'
           date='Aug 2022 - Oct 2022'
           description='Assigned in converting Figma design to a static websites. Participated in Daily Stand up Meeting where we breakdown tasks into smaller task.'
