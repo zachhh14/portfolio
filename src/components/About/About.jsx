bf093900f430d63b0432868a0a9628801738274d
zachtecson <zactec115@gmail.com>
2023-12-28 09:57:26 +0800

    adjustment on about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ZachPicture from '../../assets/zach-picture.svg';
 import './About.css';
-import { SiTailwindcss, SiCodeigniter } from 'react-icons/si';
+import { SiTailwindcss, SiCodeigniter, SiFlask } from 'react-icons/si';
 import { RiBootstrapLine } from 'react-icons/ri';
+import { FiDownload } from "react-icons/fi";
+import Resume from '/resume.pdf';
 
 function About() {
   return (
@@ -26,12 +28,14 @@ function About() {
             </span>
           </section>
           <p className='mb-2 sm:text-xl'>
-            🎓 Graduated with a Diploma in Information Communication Technology
-            from Polytechnic University of the Philippines.
+            🎓 Graduated with a Diploma in Information Communication Technology 
+            from Polytechnic University of the Philippines. Currently taking 
+            bachelor's degree in Information Technology at Polytechnic Univesity 
+            of the Philippines.
           </p>
           <p className='mb-5 sm:text-xl'>
-            💼 With an 11-month web development internship experience and
-            part-time work as a web developer, my skills include{' '}
+            💼 With 1 year of experience in internship, part-time work, and 
+            freelance as a software developer, my skills include{' '}
             <strong className='hover:underline'>
               <a
                 href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'
@@ -52,7 +56,7 @@ function About() {
             </strong>, {' '}etc.
           </p>
           <h2 className='mb-2 text-4xl font-bold'>Frameworks</h2>
-          <section className='bg-black px-5 pb-5 pt-[14px] rounded-2xl'>
+          <section className='bg-black px-5 mb-6 pb-5 pt-[14px] rounded-2xl'>
             <ul className='flex flex-wrap flex-start skills-list'>
               <li className='my-auto'>
                 <a href='https://react.dev/' target='_blank'>
@@ -105,8 +109,23 @@ function About() {
                   <i className='fa-brands fa-node-js'></i> Node JS
                 </a>
               </li>
+              <li>
+                <a 
+                  href='https://flask.palletsprojects.com/en/3.0.x/' 
+                  target='_blank'
+                  className='flex items-center'
+                >
+                  <SiFlask className='mr-1'/> Flask
+                </a>
+              </li>
             </ul>
           </section>
+          <a href={Resume} target='_blank'>
+            <button className='flex items-center p-3 mb-2 font-bold text-black bg-white rounded-xl hover:underline'>
+              <FiDownload className='mr-2 text-black'/>
+              Download Resume
+            </button>
+          </a>
         </article>
       </main>
     </div>
